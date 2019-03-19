d354eda55a831ad7e9d1d2ae1b3e58e88482bd99
Grenzen <grenzentur@rambler.ru>
2019-03-20 00:20:47 +0300

    feat: - *class Node is almost complete*

--- src/max-heap.js
+++ src/max-heap.js
@@ -1,19 +1,31 @@
 const Node = require('./node');
+//import {Node} from './node';
 
 class MaxHeap {
 	constructor() {
-		
+
+		this.root = null;
+		this.parentNodes = [];
+
 	}
 
 	push(data, priority) {
+
+		insertNode.call(new Node(data, priority));
 		
 	}
 
 	pop() {
+
+		// if(this.length !== 0) {
+
+		// }
 		
 	}
 
 	detachRoot() {
+
+		// this.root = null;
 		
 	}
 
@@ -25,15 +37,31 @@ class MaxHeap {
 		
 	}
 
-	isEmpty() {
+	isEmpty() {//it's work
+
+		if (this.root === null && this.parentNodes == 0) {
+			return true;
+		}
+		return false;
 		
 	}
 
-	clear() {
-		
+	clear() { //it's work
+
+		if (this.root !== null || this.parentNodes.length !== 0) {
+			this.root = null;
+			this.parentNodes = [];
+			return this;
+		}
 	}
 
 	insertNode(node) {
+
+		if (isEmpty(node) == 0) {
+
+			let node = new Node(data. priority);
+			return this.root = node;
+		}
 		
 	}
 
--- src/node.js
+++ src/node.js
@@ -1,23 +1,86 @@
 class Node {
 	constructor(data, priority) {
+		this.data = data;
+		this.priority = priority;
 
+		this.parent = null;
+		this.left = null;
+		this.right = null;
 	}
 
 	appendChild(node) {
+
+		// if (this.left === null) {
+		// 	return this.left = node;
+		// } else if (this.right === null) {
+		// 	return this.right = node;
+		// }
+
+		if ( this.left === null || this.right === null ) {//it's work
+
+			if ( this.left === null ) {
+
+				this.left = node;
+				node.parent = this;
+
+			} else if ( this.left !== null && this.right === null) {
+
+				this.right = node;
+				node.parent = this;
+			}
+			return this;
+		}
 
 	}
 
 	removeChild(node) {
 
+		if ( node === this.left && node.parent === this ) {//it's work
+			
+			node.parent = null;
+			this.left = null;
+		} else if ( node === this.right && node.parent === this ) {
+
+			node.parent = null;
+			this.right = null;
+		} else if ( node !== this.right && node !== this.left ) {
+			
+			throw new Error();
+		}
+		
 	}
 
-	remove() {
+	remove() {//it's work
 
+		if (this.parent !== null ) {
+			this.parent.removeChild(this);
+		}
+		
 	}
 
 	swapWithParent() {
-		
+
+		if (this.parent !== null ) {
+
+			if (this.parent.parent === null ) {
+				 
+				if ( this.parent.right === null) {
+
+					this.parent.parent = this;
+				}
+
+				this.parent.left.parent = this;
+
+			} else if ( this.parent.parent !== null ) {
+
+				this.parent = this.parent.parent;
+				this.parent.left.parent = this;
+			}
+
+		}
 	}
 }
 
 module.exports = Node;
+
+//class exports {Node};
